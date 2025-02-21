ea712498c3f3f3c7dd3d929c8487cc5000f44b91
x3zvawq <x3zvawq@gmail.com>
2025-02-21 14:32:38 +0800

    perf(design): 移动端tab显示收藏

--- src/components/tool/design/icons.vue
+++ src/components/tool/design/icons.vue
@@ -5,9 +5,13 @@
             <el-radio-group v-model="mode" class="m-icons-radio">
                 <el-radio-button label="search">探索模式</el-radio-button>
                 <el-radio-button label="view">浏览模式</el-radio-button>
+                <el-radio-button class="phone-only" label="star" v-if="isPhone() || isMiniProgram"
+                    >我的收藏</el-radio-button
+                >
             </el-radio-group>
             <IconsSearch v-if="mode === 'search'" />
             <IconsView v-if="mode === 'view'" />
+            <IconsFav v-if="mode == 'star'"></IconsFav>
         </div>
     </div>
 </template>
@@ -15,13 +19,16 @@
 <script>
 import IconsSearch from "./search.vue";
 import IconsView from "./icon_view.vue";
-// import IconsFav from "./fav.vue";
+import IconsFav from "./fav.vue";
+import { isPhone } from "@/utils";
+import { mapState } from "vuex";
+
 export default {
     name: "icons",
     props: [],
     components: {
         IconsSearch,
-        // IconsFav,
+        IconsFav,
         IconsView,
     },
     data: function () {
@@ -31,8 +38,8 @@ export default {
             mode: "search",
         };
     },
-    computed: {},
-    methods: {},
+    computed: { ...mapState(["isMiniProgram"]) },
+    methods: { isPhone },
 };
 </script>
 
